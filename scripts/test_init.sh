64defb9b2c70340199e31a4972101e4c92876cd4
Thomas Fan <thomasjpfan@gmail.com>
2017-09-30 22:50:22 -0400

    TST: Fix

--- scripts/test_init.sh
+++ scripts/test_init.sh
@@ -8,7 +8,7 @@ echo "Create stand in volume for scripts"
 docker create -v /scripts --name scripts alpine:3.6 /bin/true
 
 echo "Moving check_scaling.sh to scripts"
-docker cp $WORK_DIR/scripts/check_scaling.sh scripts:/scripts
+docker cp "${WORK_DIR}/scripts/check_scaling.sh" scripts:/scripts
 
 echo "Starting init tests"
 docker run --rm --network redis --volumes-from scripts \
