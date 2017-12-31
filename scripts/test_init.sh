51967dd2bda1038b30372d244cb7e15a2da6fb05
Thomas Fan <thomasjpfan@gmail.com>
2017-12-31 13:48:53 -0500

    ENH: Update redis to 4.0.6

--- scripts/test_init.sh
+++ scripts/test_init.sh
@@ -10,6 +10,4 @@ docker cp scripts scripts:/
 
 echo "Starting init tests"
 docker run --rm --network redis --volumes-from scripts \
-redis:4.0.2-alpine sh /scripts/check_scaling.sh 2 2
-
-
+	redis:4.0.6-alpine sh /scripts/check_scaling.sh 2 2
--- scripts/test_scale.sh
+++ scripts/test_scale.sh
@@ -9,18 +9,18 @@ export TAG=${1:-"master"}
 echo "Scale redis"
 docker service scale cache_redis-sentinel=5
 
-until [ "$(docker run --rm --network redis redis-utils:$TAG  \
-$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME value num-other-sentinels)" = "4" ]; do
-    echo "Sentinels not set up yet - sleeping"
-    sleep 2
+until [ "$(docker run --rm --network redis redis-utils:$TAG \
+	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME value num-other-sentinels)" = "4" ]; do
+	echo "Sentinels not set up yet - sleeping"
+	sleep 2
 done
 
 docker service scale cache_redis=5
 
 echo "Make sure the number of slaves are set"
 docker run --rm --network redis redis-utils:$TAG \
-$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME reset "num-slaves" 4
+	$REDIS_SENTINEL_NAME $REDIS_MASTER_NAME reset "num-slaves" 4
 
 echo "Starting following tests"
 docker run --rm --network redis --volumes-from scripts \
-redis:4.0.2-alpine sh /scripts/check_scaling.sh 4 4
+	redis:4.0.6-alpine sh /scripts/check_scaling.sh 4 4
